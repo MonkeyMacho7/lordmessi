e3305436a57fb6cc120684d2921c033386b9b9ce
MonkeyMacho7 <ethanlarson@outlook.com>
2025-01-20 21:34:26 -0700

    fixed index html

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Lord Messi</h1>
+      <p>This page is dedicated to Lionel Messi's career and achievements.</p>
+    </div>
+  );
+};
+
+export default About;
