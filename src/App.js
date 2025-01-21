a087969bf5ba192146462aa593980651478be1c8
MonkeyMacho7 <ethanlarson@outlook.com>
2025-01-20 22:33:07 -0700

    app js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import NotFound from './pages/NotFound';
+import Home from './pages/Home'; 
+import About from './pages/About'; 
+import NotFound from './pages/NotFound'; 
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        
         <Route path="/about" element={<About />} />
+        
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
