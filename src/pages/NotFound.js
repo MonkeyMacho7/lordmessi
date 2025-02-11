2e6b61b3cacec0f3ca8018d7b581398521b6565a
MonkeyMacho7 <ethanlarson@outlook.com>
2025-02-10 20:16:37 -0700

    add a youtube link for error page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -17,6 +17,15 @@ const NotFound = () => {
       <h1 className={isVisible ? "visible" : "hidden"}>404 - Page Not Found</h1>
       <p>Let's try that again. This page doesn't even exist.</p>
       <img src={errorImage} alt="Error" />
+
+	  <a 
+        href="https://www.youtube.com/watch?v=CeGOII4t52M" 
+        target="_blank" 
+        rel="noopener noreferrer" 
+        className="youtube-link"
+      >
+        I think you should watch this to re-jog your memory.
+      </a>
     </div>
   );
 };
