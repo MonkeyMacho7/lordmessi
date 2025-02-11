e1ab018f5eda6ef4bdf601e085e3d716ecab917d
MonkeyMacho7 <ethanlarson@outlook.com>
2025-02-10 23:14:43 -0700

    video

--- src/pages/Highlights.js
+++ src/pages/Highlights.js
@@ -8,13 +8,13 @@ const Highlights = () => {
       <p>Relive the greatest moments of Lionel Messi's career.</p>
 
 	  <div className="video-container">
-        <iframe
+	  <iframe
           width="560"
           height="315"
-          src="https://www.youtube.com/embed/G4wTERfoPfA?autoplay=1&mute=1&loop=1&playlist=G4wTERfoPfA"
+          src="https://www.youtube-nocookie.com/embed/G4wTERfoPfA?autoplay=1&mute=1&loop=1&playlist=G4wTERfoPfA"
           title="Messi Highlights"
           frameBorder="0"
-          allow="autoplay; encrypted-media"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
         ></iframe>
       </div>
