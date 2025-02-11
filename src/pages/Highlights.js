697b56828f5747ea0df785bd56eeb6642bc08ebf
MonkeyMacho7 <ethanlarson@outlook.com>
2025-02-10 23:31:00 -0700

    Update Highlights.js

--- src/pages/Highlights.js
+++ src/pages/Highlights.js
@@ -8,13 +8,7 @@ const Highlights = () => {
       <p>Relive the greatest moments of Lionel Messi's career.</p>
 
 	  <div className="video-container">
-	  <iframe
-          src="https://www.youtube.com/embed/waETo-ZWCRw?autoplay=1&mute=1&loop=1&playlist=waETo-ZWCRw"
-          title="Messi Highlights"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        ></iframe>
+	  <iframe width="560" height="315" src="https://www.youtube.com/embed/waETo-ZWCRw?si=4cn_uvi6gJuk5WTs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 	</div>
 	</div>
   );
