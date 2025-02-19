53fd451e670a603924826b7ac6a6b26fb1ef701f
MonkeyMacho7 <ethanlarson@outlook.com>
2025-02-18 19:25:17 -0700

    Update LiveScores.js

--- src/pages/LiveScores.js
+++ src/pages/LiveScores.js
@@ -2,26 +2,25 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "../App.css";
 
-
 const LiveScores = () => {
     const [matches, setMatches] = useState([]);
-    const apiKey = "f032a5db1eca1fdf3b0e2fab251c5e40";
-    const season = "2023";
-    const teamId = "9568"; 
+    const apiKey = "f032a5db1eca1fdf3b0e2fab251c5e40"; 
+    const season = "2024"; 
+    const teamId = "9568";  
     const leagueId = "253"; 
 
     useEffect(() => {
         const fetchMatches = async () => {
             try {
                 const response = await axios.get(
-                    `https://v3.football.api-sports.io/fixtures?season=2023&team=9568&league=253&last=5`,
+                    `https://v3.football.api-sports.io/fixtures`,
                     {
                         params: {
                             season: season,
                             team: teamId,
                             league: leagueId,
-							from: "2023-04-01", 
-            				to: "2023-07-31", 
+                            last: 2, 
+                            next: 3, 
                         },
                         headers: {
                             "x-apisports-key": apiKey,
@@ -52,12 +51,13 @@ const LiveScores = () => {
                 <ul>
                     {matches.map((match, index) => (
                         <li key={index}>
-                            {match.teams.home.name} {match.goals.home} - {match.goals.away} {match.teams.away.name}
+                            {match.teams.home.name} {match.goals.home} - {match.goals.away} {match.teams.away.name} <br />
+                            <small>{match.fixture.date}</small>
                         </li>
                     ))}
                 </ul>
             ) : (
-                <p>No recent matches found.</p>
+                <p>No recent or upcoming matches found.</p>
             )}
         </div>
     );
