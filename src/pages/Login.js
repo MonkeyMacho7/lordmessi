5bc641b6905fe75aa5784adee2833e80df63fb91
MonkeyMacho7 <ethanlarson@outlook.com>
2025-03-03 19:18:31 -0700

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,40 +1,50 @@
 import React, { useState } from "react";
 import { useAuth } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 import "../App.css";
 
 const Login = () => {
     const { login } = useAuth();
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+    const navigate = useNavigate();
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        login(username, password);
+        setIsLoading(true);
+        setTimeout(() => {
+            login(username, password);
+            navigate("/livescores");
+        }, 2000);
     };
 
     return (
-    	<div className="login-container">
-			<h2 className="login-title">Login</h2>
-			<p className="login-subtitle">
-			Please enter your credentials to access the site.
-		</p>
-		<form onSubmit={handleSubmit} className="login-box">
-			<input
-				type="text"
-				placeholder="Username"
-				value={username}
-				onChange={(e) => setUsername(e.target.value)}
-			/>
-			<input
-				type="password"
-				placeholder="Password"
-				value={password}
-				onChange={(e) => setPassword(e.target.value)}
-			/>
-			<button type="submit">Login</button>
-		</form>
-	</div>
-
+        <div className="login-container">
+            <h2 className="login-title">Login</h2>
+            <p className="login-subtitle">
+                Please enter your credentials to access the site.
+            </p>
+            {isLoading ? (
+                <p className="loading-text">Logging in...</p>
+            ) : (
+                <form onSubmit={handleSubmit} className="login-box">
+                    <input
+                        type="text"
+                        placeholder="Username"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <button type="submit">Login</button>
+                </form>
+            )}
+        </div>
     );
 };
 
