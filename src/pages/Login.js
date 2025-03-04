5be06953898963a71c874f1e59d45802ab762879
MonkeyMacho7 <ethanlarson@outlook.com>
2025-03-03 18:13:47 -0700

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -14,26 +14,26 @@ const Login = () => {
 
     return (
     	<div className="login-container">
-    	<h2 className="login-title">Login</h2>
-   		 <p className="login-subtitle">
-        Please enter your credentials to access the site.
-    </p>
-    <form onSubmit={handleSubmit} className="login-box">
-        <input
-            type="text"
-            placeholder="Username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-        />
-        <input
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit">Login</button>
-    </form>
-</div>
+			<h2 className="login-title">Login</h2>
+			<p className="login-subtitle">
+			Please enter your credentials to access the site.
+		</p>
+		<form onSubmit={handleSubmit} className="login-box">
+			<input
+				type="text"
+				placeholder="Username"
+				value={username}
+				onChange={(e) => setUsername(e.target.value)}
+			/>
+			<input
+				type="password"
+				placeholder="Password"
+				value={password}
+				onChange={(e) => setPassword(e.target.value)}
+			/>
+			<button type="submit">Login</button>
+		</form>
+	</div>
 
     );
 };
