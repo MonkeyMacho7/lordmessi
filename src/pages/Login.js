4a3c5f866acced49f56d54f7d018ab74fc113fbd
MonkeyMacho7 <ethanlarson@outlook.com>
2025-03-03 18:00:15 -0700

    oops

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useAuth } from "../context/AuthContext";
-import "../styles/Login.css";
+import "../App.css";
 
 const Login = () => {
     const { login } = useAuth();
