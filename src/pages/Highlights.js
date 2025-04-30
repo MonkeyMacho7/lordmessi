2079a09116ecf2822b1673d0ccce21e406103d13
MonkeyMacho7 <ethanlarson@outlook.com>
2025-04-30 07:11:15 -0600

    polish
    polish

--- src/pages/Highlights.js
+++ src/pages/Highlights.js
@@ -1,16 +1,43 @@
 import React from "react";
 import "../App.css";
 
+const videoData = [
+  {
+    title: "The Encara Messi",
+    src: "https://www.youtube.com/embed/waETo-ZWCRw?si=4cn_uvi6gJuk5WTs"
+  },
+  {
+    title: "The World Cup Triumph",
+    src: "https://www.youtube.com/embed/ZmKy_fnRM_E?start=5"
+  },
+  {
+    title: "The King of Football",
+    src: "https://www.youtube.com/embed/p693u53Q10U?start=34"
+  }
+];
+
 const Highlights = () => {
   return (
     <div className="highlights">
       <h1>Enjoy...</h1>
       <p>Relive the greatest moments of Lionel Messi's career.</p>
 
-	  <div className="video-container">
-	  <iframe width="560" height="315" src="https://www.youtube.com/embed/waETo-ZWCRw?si=4cn_uvi6gJuk5WTs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-	</div>
-	</div>
+      {videoData.map((video, index) => (
+        <div className="video-section" key={index}>
+          <h2 className="video-title">{video.title}</h2>
+          <div className="video-container">
+            <iframe
+              src={video.src}
+              title={video.title}
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowFullScreen
+              referrerPolicy="strict-origin-when-cross-origin"
+            ></iframe>
+          </div>
+        </div>
+      ))}
+    </div>
   );
 };
 
